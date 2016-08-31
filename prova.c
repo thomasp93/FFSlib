6eb3da4e685509c81a730cc81af27475e68c6fb3
thomasp93 <pertilethomas@gmail.com>
2016-08-31 15:37:33 +0200

    test prova

--- prova.c
+++ prova.c
@@ -4,6 +4,7 @@ int main(void)
 {
 	Disk_Create(); // creazione del disco
 	FS_Init("pips.img"); // creazione disco
-	Dir_Create("/home"); // creazione cartella home
+	if(Dir_Create("/home")!=0) printf("Errore /home"); // creazione cartella home
+	if(Dir_Create("/opt")!=0) printf("Errore /opt");
 	FS_Sync("pips.img"); // salvataggio disco
 }
